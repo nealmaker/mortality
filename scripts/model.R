2955a512ec0b46044fb83ae470a14abdeb310070
Neal Maker <nealmaker@gmail.com>
2020-02-06 14:35:37 -0500

    update operational model

--- scripts/model.R
+++ scripts/model.R
@@ -73,28 +73,28 @@ y_sub <- train_sub[,1]
 # Model testing
 # This one needs y to be a factor with two levels (not named "0" and "1")
 # to predict class probabilities: predict(model, newdata, type = "prob")
-ranger_mod_test <- train(x_sub, y_sub,
-                         method = "ranger",
-                         preProcess = c("center", "scale", "YeoJohnson"),
-                         trControl = trainControl(classProbs = T),
-                         num.trees = 50,
-                         tuneGrid = data.frame(.mtry = seq(2, 14, by = 4),
-                                               .splitrule = rep("gini", 4),
-                                               .min.node.size = rep(3, 4)),
-                         importance = 'impurity')
+# ranger_mod_test <- train(x_sub, y_sub,
+#                          method = "ranger",
+#                          preProcess = c("center", "scale", "YeoJohnson"),
+#                          trControl = trainControl(classProbs = T),
+#                          num.trees = 50,
+#                          tuneGrid = data.frame(.mtry = seq(2, 14, by = 4),
+#                                                .splitrule = rep("gini", 4),
+#                                                .min.node.size = rep(3, 4)),
+#                          importance = 'impurity')
 
 # this is set up as right-censored, should be interval;
 # looks like ranger only works for right censored data though...
 # chf shows prob of death at various times for each observation
 # and unique.death.times are the times that correspond to those probs.
-ranger_test <- ranger(y_sub ~ spp + dbh + cr, data = train_sub)
-
-randomForest_mod_test <- train(x_sub, y_sub,
-                               method = "rf",
-                               preProcess = c("center", "scale", "YeoJohnson"),
-                               ntree = 200,
-                               importance = T,
-                               tuneGrid = data.frame(mtry = seq(2, 14, by = 4)))
+# ranger_test <- ranger(y_sub ~ spp + dbh + cr, data = train_sub)
+# 
+# randomForest_mod_test <- train(x_sub, y_sub,
+#                                method = "rf",
+#                                preProcess = c("center", "scale", "YeoJohnson"),
+#                                ntree = 200,
+#                                importance = T,
+#                                tuneGrid = data.frame(mtry = seq(2, 14, by = 4)))
 
 # "rf" works on subsample, but "cannot allocate vector of 2.3 Gb" 
 # with full (80%) training set. Try decreasing size of training set.
@@ -107,7 +107,7 @@ mort_model_full <- train(x, y,
                          method = "ranger",
                          preProcess = c("center", "scale", "YeoJohnson"),
                          trControl = trainControl(classProbs = T),
-                         num.trees = 2000,
+                         num.trees = 200,
                          tuneGrid = data.frame(.mtry = seq(2, 14, by = 4),
                                                .splitrule = rep("gini", 4),
                                                .min.node.size = rep(10, 4)),
@@ -121,22 +121,22 @@ mort_model_full <- train(x, y,
 #                                                 minNode = rep(2, 4)))
 
 
-# Final model
-set.seed(1)
-mort_model_2 <- train(x, y,
-                    method = "Rborist",
-                    tuneGrid = data.frame(predFixed = seq(2, 8, by = 2),
-                                          minNode = rep(2, 4)))
+# Rborist version
+# set.seed(1)
+# mort_model_2 <- train(x, y,
+#                     method = "Rborist",
+#                     tuneGrid = data.frame(predFixed = seq(2, 8, by = 2),
+#                                           minNode = rep(2, 4)))
 
 # validation$census can be used to get probabilities
 # validation$yPred seems to have changed factor levels
-mort_model <- Rborist(x = x, y = y, 
-                      predFixed = 7,
-                      minNode = 2)
+# mort_model <- Rborist(x = x, y = y, 
+#                       predFixed = 7,
+#                       minNode = 2)
 
 
 #####################################################################
-# Results 
+# Results full
 #####################################################################
 
 y_hat_train <- predict(mort_model, newdata = x)
@@ -149,7 +149,7 @@ caret::F_meas(data = y_hat_train, reference = y, positive = "1")
 
 
 #####################################################################
-# Prediction 
+# Prediction full
 #####################################################################
 
 df <- data.frame(spp = factor(c("hard maple", "paper birch"), 
@@ -166,17 +166,54 @@ df <- data.frame(spp = factor(c("hard maple", "paper birch"),
 predict(mort_model_full, newdata = df, type = "prob")
 
 #this will give a probability of death (over next 5ish years)
-predict(mort_model, newdata = df_trans)$census[,2]/500
+# predict(mort_model_full, newdata = df_trans)$census[,2]/500
 
 
 #####################################################################
 # Test
 #####################################################################
 
-y_hat_test <- predict(mort_model, newdata = test_tran[,-1])$yPred
-y_hat_test <- as.factor(if_else(y_hat_test == 1, 0, 1))
+# y_hat_test <- predict(mort_model, newdata = test_tran[,-1])$yPred
+# y_hat_test <- as.factor(if_else(y_hat_test == 1, 0, 1))
+# 
+# confusionMatrix(data = y_hat_test, reference = test$died, positive = "1")
 
-confusionMatrix(data = y_hat_test, reference = test$died, positive = "1")
+
+#####################################################################
+# Train operational model
+#####################################################################
+
+x2 <- select(x, 
+             -landscape, 
+             -crown_class, 
+             -tree_class, 
+             -aspect, 
+             -slope, 
+             -stocking)
+
+memory.limit(size=56000)
+set.seed(1)
+mort_model_op <- 
+  train(x2, y,
+        method = "ranger",
+        preProcess = c("center", "scale", "YeoJohnson"),
+        trControl = trainControl(classProbs = T),
+        num.trees = 200,
+        importance = 'impurity',
+        tuneGrid = data.frame(.mtry = seq(2, 10, by = 2),
+                              .splitrule = rep("variance", 5),
+                              .min.node.size = rep(5, 5)))
+
+
+#####################################################################
+# Results operational model
+#####################################################################
+
+mort_model_op$results
+
+plot(mort_model_op)
+
+varImp(mort_model_op, scale = F)
 
 
 #####################################################################
@@ -184,4 +221,5 @@ confusionMatrix(data = y_hat_test, reference = test$died, positive = "1")
 #####################################################################
 
 # STOP! Too big to fit on GitHub
-# save(mort_model, file = "rda/mort_model.rda")
+save(mort_model_full, file = "../big-rdas/mort-model-full.rda")
+save(mort_model_op, file = "../big-rdas/mort-model-op.rda")
